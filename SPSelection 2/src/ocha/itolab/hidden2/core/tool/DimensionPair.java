7a5060349734315925e05a2bfe54fae280e00c57
kako-gan <89618333+yuyamimi@users.noreply.github.com>
2023-09-04 07:50:29 +0900

    Deleted Unuse Files

--- SPSelection 2/src/ocha/itolab/hidden2/core/tool/DimensionPair.java
+++
@@ -1,7 +0,0 @@
-package ocha.itolab.hidden2.core.tool;
-
-public class DimensionPair {
-	int id, id1, id2;
-	double r;
-	double[] score = new double[4];
-}
--- SPSelection 3/src/ocha/itolab/hidden2/datagen/youtuber/Movie.java
+++
@@ -1,7 +0,0 @@
-package ocha.itolab.hidden2.datagen.youtuber;
-
-public class Movie {
-	int length, date, numplay, higheval, loweval, comments,
-		hashtags, colab, gender, numperson, professional;
-	String name, youtuber, url;	
-}
