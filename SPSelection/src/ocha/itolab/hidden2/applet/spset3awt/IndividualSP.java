5cc3188c4603fcc497e047f79ea445345e16a622
kako-gan <89618333+yuyamimi@users.noreply.github.com>
2023-09-03 13:01:29 +0900

    Added upside down

--- SPSelection/src/ocha/itolab/hidden2/applet/spset3awt/IndividualSP.java
+++ SPSelection/src/ocha/itolab/hidden2/applet/spset3awt/IndividualSP.java
@@ -27,16 +27,13 @@ public class IndividualSP {
 	float brightness = 0.75f;
 	double SHRINK = 0.925;
 	Graphics2D g2;
+	boolean hideGrayPoints = true;
 
 	/*****/
 	int psnum = 0;
 	double[][][][] points;
 	double vrad;
-	double[][][] varray = null, barray = null, tarray = null;
-	ArrayList<double[]> varraylist, barraylist, tarraylist;
-	boolean outlierVertices = true, outlierBoundaries = true, outlierTriangles = true, hideGrayPoints = true;
-	boolean isOutlier2 = false;
-
+	
 	/*****/
 
 	/**
@@ -52,15 +49,12 @@ public class IndividualSP {
 
 	/*****/
 	public void changeOutlierVertices() {
-		outlierVertices = !outlierVertices;
 	}
 
 	public void changeOutlierBoundaries() {
-		outlierBoundaries = !outlierBoundaries;
 	}
 
 	public void changeOutlierTriangles() {
-		outlierTriangles = !outlierTriangles;
 	}
 
 	public void hideGrayPoints() {
@@ -68,44 +62,12 @@ public class IndividualSP {
 	}
 
 	public void setIsOutlier2(boolean isOut) {
-		isOutlier2 = isOut;
 	}
 
 	public void clearArray() {
-		if (varraylist != null) {
-			varraylist.clear();
-		}
-		if (barraylist != null) {
-			barraylist.clear();
-		}
-		if (tarraylist != null) {
-			tarraylist.clear();
-		}
 	}
 
-	public void setVarray(ArrayList<double[]> v_array) {
-		if (varraylist == null) {
-			varraylist = v_array;
-		} else {
-			varraylist.addAll(v_array);
-		}
-	}
-
-	public void setBarray(ArrayList<double[]> b_array) {
-		if (barraylist == null) {
-			barraylist = b_array;
-		} else {
-			barraylist.addAll(b_array);
-		}
-	}
-
-	public void setTarray(ArrayList<double[]> t_array) {
-		if (tarraylist == null) {
-			tarraylist = t_array;
-		} else {
-			tarraylist.addAll(t_array);
-		}
-	}
+	
 
 	public void setValueIdSet(ArrayList<int[]> list) {
 		vlist = list;
@@ -118,6 +80,7 @@ public class IndividualSP {
 	public void setNumClusters(int n) {
 		numclusters = n;
 	}
+
 
 	/*****/
 	public IndividualSet getIndividualSet() {
@@ -135,7 +98,7 @@ public class IndividualSP {
 	/******/
 
 	public void draw(Graphics2D gg2, double scl) {
-
+		
 		if (iset == null || vlist == null)
 			return;
 		g2 = gg2;
@@ -153,16 +116,12 @@ public class IndividualSP {
 		/*****/
 		for (int i = 0; i < ncell; i++) {
 			double y = ((double) i + 0.5) * size - 1.0;
+			y *= -1.0;
 			for (int j = 0; j < ncell; j++, count++) {
 				if (count >= vlist.size())
 					return;
-				if (i % 2 == 0) {
-					double x = ((double) j + 0.5) * size - 1.0;
-					drawOneSP(x, y, size, count);
-				} else if (i % 2 != 0) {
-					double x = ((double) (ncell - j - 1) + 0.5) * size - 1.0;
-					drawOneSP(x, y, size, count);
-				}
+				double x = ((double) j + 0.5) * size - 1.0;
+				drawOneSP(x, y, size, count);
 			}
 		}
 	}
@@ -182,7 +141,6 @@ public class IndividualSP {
 		g2.setStroke(new BasicStroke(1.0f));
 
 		p1 = du.transformPosition(minx, maxy, 0.0, 1);
-
 		p2 = du.transformPosition(minx, miny, 0.0, 2);
 		p3 = du.transformPosition(maxx, miny, 0.0, 3);
 		p4 = du.transformPosition(maxx, maxy, 0.0, 4);
@@ -198,15 +156,8 @@ public class IndividualSP {
 		double rgb[] = new double[3];
 
 		IndividualSet ps = iset;
+		
 
-		// System.out.println(vlist.size());
-		/*****/
-		// if(psnum == 0) {
-		// psnum = ps.getNumIndividual();
-		// }
-		// points = new double[psnum][3][ps.getNumExplain() * ps.getNumObjective()];
-		// }
-		/*****/
 
 		// Draw gray dots
 		for (int i = 0; i < ps.getNumIndividual(); i++) {
@@ -227,8 +178,10 @@ public class IndividualSP {
 				double x = (ex[xposId] - ps.explains.min[xposId]) / (ps.explains.max[xposId] - ps.explains.min[xposId]);
 				double y = (ob[yposId] - ps.objectives.min[yposId])
 						/ (ps.objectives.max[yposId] - ps.objectives.min[yposId]);
+				y *= -1.0;
 				x = (x - 0.5) * size * SHRINK + sx;
-				y = (y - 0.5) * size * SHRINK + sy;
+				y = (y + 0.5) * size * SHRINK + sy;
+				
 				p1 = du.transformPosition(x, y, 0.0, 1);
 
 				polygon = new GeneralPath(GeneralPath.WIND_EVEN_ODD, 2);
@@ -240,7 +193,7 @@ public class IndividualSP {
 						polygon.moveTo((int) p1[0], (int) p1[1]);
 					else
 						polygon.lineTo((int) p1[0], (int) p1[1]);
-					// System.out.println("1: i=" + i + " j=" + j + " x=" + p1[0] + " y=" + p1[1]);
+					//System.out.println("1:   i=" + i + " j=" + j + " x=" + p1[0] + " y=" + p1[1]);
 				}
 				g2.fill(polygon);
 
@@ -284,8 +237,10 @@ public class IndividualSP {
 			double x = (ex[xposId] - ps.explains.min[xposId]) / (ps.explains.max[xposId] - ps.explains.min[xposId]);
 			double y = (ob[yposId] - ps.objectives.min[yposId])
 					/ (ps.objectives.max[yposId] - ps.objectives.min[yposId]);
+			y *= -1.0;
 			x = (x - 0.5) * size * SHRINK + sx;
-			y = (y - 0.5) * size * SHRINK + sy;
+			y = (y + 0.5) * size * SHRINK + sy;
+			
 			/*****/
 			int clusterId = p.getClusterId();
 			if (clusterId == -1) {
@@ -309,29 +264,7 @@ public class IndividualSP {
 				// points[i][1][pid][j]);
 			}
 
-			boolean isOutlier = false;
-			if (isOutlier2 == true) {
-				if (varraylist != null) {
-					for (int j = 0; j < varraylist.size(); j++) {
-						if ((double) pid == varraylist.get(j)[3]) {
-							if (clusterId == varraylist.get(j)[4]) {
-								if (varraylist.get(j)[0] == x && varraylist.get(j)[1] == y) {
-									isOutlier = true;
-									continue;
-								}
-							}
-						}
-					}
-				}
-
-			}
-			/*****/
-
-			vrad = (isPicked == true) ? (VRAD * 1.5) : VRAD;
-			if (outlierVertices) {
-				vrad = (isOutlier == true) ? (vrad * 2.5) : vrad;
-			}
-
+			
 			polygon = new GeneralPath(GeneralPath.WIND_EVEN_ODD, 2);
 			for (int j = 0; j < NUMV; j++) {
 				double xx = vrad * Math.cos(2.0 * Math.PI * ((double) j / NUMV)) + x;
@@ -341,58 +274,19 @@ public class IndividualSP {
 					polygon.moveTo((int) p1[0], (int) p1[1]);
 				else
 					polygon.lineTo((int) p1[0], (int) p1[1]);
-				// System.out.println("2: i=" + i + " j=" + j + " xx=" + xx + " yy=" + yy + "
-				// x=" + p1[0] + " y=" + p1[1]);
-
+				//System.out.println("2:   i=" + i + " j=" + j + " xx=" + xx + " yy=" + yy + " x=" + p1[0] + " y=" + p1[1]);
+				
 			}
 			g2.fill(polygon);
 
 		}
 		flag = false;
 
-		/*****/
-		/*
-		 * if(isOutlier2 == true) { if(outlierBoundaries) { if(barraylist != null) {
-		 * rgb[0] = rgb[1] = rgb[2] = 0.25;
-		 * 
-		 * gl2.glBegin(GL.GL_LINES); for(int j = 0; j < barraylist.size(); j++) {
-		 * if(numclusters >= 2) { rgb = calcNodeColor((int)(barraylist.get(j)[7]),
-		 * numclusters, 0.75f, 0.50f); } gl2.glColor4d(rgb[0], rgb[1], rgb[2], 0.5);
-		 * if((double)pid == barraylist.get(j)[6]) {
-		 * gl2.glVertex3d(barraylist.get(j)[0], barraylist.get(j)[1], 0.0);
-		 * gl2.glVertex3d(barraylist.get(j)[3], barraylist.get(j)[4], 0.0);
-		 * //System.out.println("barraylist [0]: " + barraylist.get(j)[0] + ", [1]: " +
-		 * barraylist.get(j)[1] + ", [3]: " + barraylist.get(j)[3] + ", [4]: " +
-		 * barraylist.get(j)[4] + ", [6]: " + barraylist.get(j)[6] + ", [7]: " +
-		 * barraylist.get(j)[7]); } } gl2.glEnd(); } } if(outlierTriangles) {
-		 * if(tarraylist != null) { rgb[0] = rgb[1] = rgb[2] = 0.5;
-		 * gl2.glBegin(GL.GL_TRIANGLES); for(int j = 0; j < tarraylist.size(); j++) {
-		 * if((double)pid == tarraylist.get(j)[9]) { double x1 = tarraylist.get(j)[0];
-		 * double y1 = tarraylist.get(j)[1]; double x2 = tarraylist.get(j)[3]; double y2
-		 * = tarraylist.get(j)[4]; double x3 = tarraylist.get(j)[6]; double y3 =
-		 * tarraylist.get(j)[7]; double area = 100000 * Math.abs((x1 - x3) * (y2 - y3) -
-		 * (x2 - x3) * (y1 -y3)); //if(area > 150) { rgb[0] = rgb[1] = rgb[2] = 0.9;
-		 * if(numclusters >= 2) { rgb = calcNodeColor((int)(tarraylist.get(j)[10]),
-		 * numclusters, 0.25f, 1.00f); } gl2.glColor4d(rgb[0], rgb[1], rgb[2], 0.3); }
-		 * if(area > 60 && area <= 150) { rgb[0] = rgb[1] = rgb[2] = 0.8; if(numclusters
-		 * >= 2) { rgb = calcNodeColor((int)(tarraylist.get(j)[10]), numclusters, 0.25f,
-		 * 0.90f); } gl2.glColor4d(rgb[0], rgb[1], rgb[2], 0.3); } if(area > 30 && area
-		 * <= 60) { rgb[0] = rgb[1] = rgb[2] = 0.7; if(numclusters >= 2) { rgb =
-		 * calcNodeColor((int)(tarraylist.get(j)[10]), numclusters, 0.25f, 0.80f); }
-		 * gl2.glColor4d(rgb[0], rgb[1], rgb[2], 0.3); } if(area > 0 && area <= 30) {
-		 * rgb[0] = rgb[1] = rgb[2] = 0.6; if(numclusters >= 2) { rgb =
-		 * calcNodeColor((int)(tarraylist.get(j)[10]), numclusters, 0.25f, 0.70f); }
-		 * gl2.glColor4d(rgb[0], rgb[1], rgb[2], 0.3); }
-		 * 
-		 * gl2.glVertex3d(x1, y1, 0.0); gl2.glVertex3d(x2, y2, 0.0); gl2.glVertex3d(x3,
-		 * y3, 0.0); } } gl2.glEnd(); } } }
-		 */
-
 		g2.setPaint(new Color(0, 0, 0));
 		g2.setStroke(new BasicStroke(1.0f));
 
 		double x1 = 0.0 * size + sx;
-		double y1 = -0.5 * size + sy;
+		double y1 = 0.5 * size + sy;
 		p1 = du.transformPosition(x1, y1, 0.0, 1);
 		writeOneString(p1[0], p1[1], ps.getValueName(xposId));
 		double x2 = -0.5 * size + sx;
@@ -416,7 +310,7 @@ public class IndividualSP {
 	void writeOneString(double x, double y, String word) {
 		if (word == null || word.length() <= 0)
 			return;
-		Font font = new Font("MSï¿½Sï¿½Vï¿½bï¿½N", Font.BOLD, 11);
+		Font font = new Font("MSƒSƒVƒbƒN", Font.BOLD, 11);
 		g2.setFont(font);
 		g2.drawString(word, (int) p1[0], (int) p1[1]);
 	}
